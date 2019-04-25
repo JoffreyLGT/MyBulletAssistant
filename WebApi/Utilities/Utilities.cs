e230c7c377ed3c308f0a4ada9321c32bd5584d5f
Joffrey LAGUT <j.lagut@live.fr>
2019-04-25 21:40:47 +0200

    Fix CompareIdWithTokenId to return the expected result

--- WebApi/Utilities/Utilities.cs
+++ WebApi/Utilities/Utilities.cs
@@ -5,10 +5,20 @@ namespace WebApi.Utilities
 {
     public static class AuthentificationUtils
     {
+        /// <summary>
+        /// Compare the user id included in the token with the userId provided.
+        /// </summary>
+        /// <param name="userFromContext"></param>
+        /// <param name="userId"></param>
+        /// <returns>true if they are the same</returns>
         public static bool CompareIdWithTokenId(this ClaimsPrincipal userFromContext, string userId)
         {
             string tokenUserId = userFromContext.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
-            return (string.IsNullOrWhiteSpace(tokenUserId) || !tokenUserId.Equals(userId));
+            if (string.IsNullOrWhiteSpace(tokenUserId))
+            {
+                return false;
+            }
+            return tokenUserId.Equals(userId);
         }
     }
 }
